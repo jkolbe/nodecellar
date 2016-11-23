469532058a83672df5ed2e41810f709e21a472c1
Joanna Kolbe <joannakolbe@gmail.com>
2016-11-23 10:09:18 -0500

    seperate routs

--- README.md
+++ README.md
@@ -5,3 +5,12 @@ Creating a REST API using Node.js, Express, and MongoDB
 
 2. Run server 
 	> node server.js
+
+3. Test results
+	
+	Get all the wines in the database:
+		http://localhost:3000/wines
+	
+	Get wine with a specific id (for example: 1):
+		http://localhost:3000/wines/1
+
--- server.js
+++ server.js
@@ -1,6 +1,17 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(3000, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:3000/');
+// var http = require('http');
+// http.createServer(function (req, res) {
+//   res.writeHead(200, {'Content-Type': 'text/plain'});
+//   res.end('Hello World\n');
+// }).listen(3000, '127.0.0.1');
+// console.log('Server running at http://127.0.0.1:3000/');
+
+var express = require('express'),
+	wines = require('./routes/wines');
+
+var app = express();
+
+app.get('/wines', wines.findAll);
+app.get('/wines/:id', wines.findById);
+
+app.listen(3000);
+console.log('Listening on port 3000...');
