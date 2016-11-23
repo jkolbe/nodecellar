69e413b657f38cd59d89828118c26e73f5027802
Joanna Kolbe <joannakolbe@gmail.com>
2016-11-23 10:42:28 -0500

    mongodb configs and insert sample data

--- routes/wines.js
+++ routes/wines.js
@@ -1,3 +1,25 @@
+var mongo = require('mongodb');
+
+var Server = mongo.Server,
+	Db = mongo.Db,
+	BSON = mongo.BSONPure;
+
+var server = new Server('localhost', 27017, {auto_reconnect: true});
+db = new Db('winedb', server);
+
+db.open(function(err, db){
+	if(!err) {
+		console.log("Connected to 'winedb' databse");
+		db.collection('wines', {strict:true}, function(err, collection) {
+			if (err) {
+                console.log("The 'wines' collection doesn't exist. Creating it with sample data...");
+                // populate with data
+                populateDB();
+            }
+		});
+	}
+});
+
 exports.findAll = function(req, res) {
 	res.send([{name:'wine1'}, {name:'wine2'}, {name:'wine3'}]);
 };
@@ -9,4 +31,33 @@ exports.findById = function(req, res) {
 exports.addWine = function(req, res) {}
 exports.updateWine = function(req, res) {}
 exports.deleteWine = function(req, res) {}
+
+
+// populate DB with sample data
+var populateDB = function() {
+	var wines = [
+    {
+        name: "CHATEAU DE SAINT COSME",
+        year: "2009",
+        grapes: "Grenache / Syrah",
+        country: "France",
+        region: "Southern Rhone",
+        description: "The aromas of fruit and spice...",
+        picture: "saint_cosme.jpg"
+    },
+    {
+        name: "LAN RIOJA CRIANZA",
+        year: "2006",
+        grapes: "Tempranillo",
+        country: "Spain",
+        region: "Rioja",
+        description: "A resurgence of interest in boutique vineyards...",
+        picture: "lan_rioja.jpg"
+    }];
+
+    db.collection('wines', function(err, collection){
+    	collection.insert(wines, {safe:true}, function(err, result) {});
+    });
+};
+
 
